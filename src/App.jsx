cd7cbb65213f19c0a5f439958aa21a52493ff22e
ruchap24 <patilrucha991@gmail.com>
2025-02-21 18:13:15 +0530

    add Sidebar component and integrate it into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
+   <Sidebar />   
     </BrowserRouter>
   );
 }
