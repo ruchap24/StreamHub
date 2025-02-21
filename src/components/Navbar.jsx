bed62e9c85db92733ab64ac9ad85345cfd83d38a
ruchap24 <patilrucha991@gmail.com>
2025-02-21 16:01:17 +0530

    add search and menu functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,27 @@
+import { AiOutlineMenu } from "react-icons/ai";
+
+import { CiSearch } from "react-icons/ci";
+import { IoMdMic } from "react-icons/io";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className="flex justify-between px-6 py-2">
+      <div className="flex items-centerspace-x-4 border border-black">
+        <AiOutlineMenu className="text-xl cursor-pointer" />
+        
+      </div>
+      <div className="flex w-[35%] border border-black">
+        <div className="w-[100%]">
+          <input type="text" placeholder="Search" className="outline-null " />
+        </div>
+        <button>
+          <CiSearch />
+        </button>
+        <IoMdMic />
+      </div>
+      <div className="border border-black">3</div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
