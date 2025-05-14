803ac066f9fdf9194ca8b8cb103a53fe43f5d73d
ruchap24 <patilrucha991@gmail.com>
2025-05-14 17:29:38 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import { BrowserRouter } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Search from "./components/Search";
+import PlayingVideo from "./components/PlayingVideo";
+import { useAuth } from "./context/AuthProvider";
+import Loading from "./loader/Loading";
 
 function App() {
+  const { loading } = useAuth();
   return (
-    <BrowserRouter>
+    <div>
+      {loading && <Loading />}
       <Navbar />
-   <Sidebar />   
-    </BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/search/:searchQuery" element={<Search />} />
+        <Route path="/video/:id" element={<PlayingVideo />} />
+      </Routes>
+    </div>
   );
 }
 
